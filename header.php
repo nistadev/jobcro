c2c3d07f1f6ada1564223354278294bbb1925b71
Patrik Nista <subgeo98@Patrick-Nistas-MacBook.local>
2017-05-09 22:16:38 +0200

    First Commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TIME - Your Professional Job Timer</title>
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<header>
+    
+</header>
+<nav class="navbar navbar-default navbar-static-top" role="navigation">
+    <div class="container">
+        <!-- <a class="navbar-brand" href="index.php?page=inici">TIME</a> -->
+        <ul class="nav navbar-nav text-center">
+            <li>
+                <a href="index.php?page=entra_hores">Entrar Hores</a>
+            </li>
+            <li>
+                <a href="index.php?page=registres">Registres</a>
+            </li>
+        </ul>
+    </div>
+</nav>

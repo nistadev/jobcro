1b41a81c98f254167ab74cf5b2283c1262e78910
Patriciu Nista <subgeo98@gmail.com>
2017-05-19 02:24:27 +0200

    Angular implementation, quickstart almost finish

--- timeapp/src/app/app.component.ts
+++ timeapp/src/app/app.component.ts
@@ -1,20 +1,9 @@
 import { Component } from '@angular/core';
 
-export class Hero {
-  id: number;
-  name: string;
-}
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html'
 })
-
 export class AppComponent {
-    title = 'Tour of Heroes';
-    hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    };
+	title = "Heroes";
 }
